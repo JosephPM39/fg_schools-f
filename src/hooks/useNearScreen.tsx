cc8091a4319f2a45e994836eee616327d8225674
JosephPM39 <josias.jjpm49@hotmail.com>
2023-01-25 16:06:27 -0600

    IntersectionObserver hook

---
+++ src/hooks/useNearScreen.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useRef, useState } from "react"
+
+export const useNearScreen = () => {
+  const [show, setShow] = useState(false)
+  const element = useRef(null)
+
+  useEffect(() => {
+    if (!element.current) return
+    const observer = new window.IntersectionObserver((entries) => {
+      const {isIntersecting} = entries[0]
+      if (isIntersecting) {
+        setShow(true)
+        observer.disconnect()
+      }
+    })
+    observer.observe(element.current)
+  }, [element])
+
+  return {
+    show,
+    element,
+    setShow
+  }
+}
