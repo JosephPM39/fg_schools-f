440b7507cb2b33c663b95b680a3f8aeaa71ae0c9
JosephPM39 <josias.jjpm49@hotmail.com>
2023-03-15 14:28:22 -0600

    Refactor to useBase's pagination functions, now it's more efficient and fast

--- src/hooks/api/useBase.tsx
+++ src/hooks/api/useBase.tsx
@@ -6,6 +6,7 @@ import { filterBy } from '../../api/services/utils'
 import { useDebounce } from '../useDebouce'
 import { useNetStatus, AppNetStatus } from '../useNetStatus'
 import { SearchBy, SearchById } from '../../api/services/types'
+import { ErrorCatched } from '../../api/handlers/errors'
 
 export interface BaseParams<Model extends IBaseModel> {
   path: string
@@ -14,7 +15,7 @@ export interface BaseParams<Model extends IBaseModel> {
 }
 
 type FetchParams<Model extends IBaseModel> = {
-  mode?: 'clean' | 'merge'
+  mode?: 'clean' | 'merge' | 'info'
 } & ReadParams<Model>
 
 interface FetchNextParams {
@@ -26,9 +27,47 @@ type CreateParams<Model extends IBaseModel> = Model | Model[]
 
 const secureMerge = <T extends IBaseModel>(listOne: T[], listTwo: T[]) => {
   const list = [...listOne, ...listTwo]
-  return list.filter((item, index) => {
+  const filtred = list.filter((item, index) => {
     return list.findIndex((s) => s.id === item.id) === index
   })
+  console.log('MERGING:',
+    'F:', listOne.length,
+    'FM:', listOne.at(listOne.length - 1)?.id?.slice(0, 3),
+    'S:', listTwo.length,
+    'SM:', listTwo.at(0)?.id?.slice(0, 3),
+    'MERGED:', list.length,
+    'FILTERED:', filtred.length
+  )
+  return filtred
+}
+
+type FetchNextStatus<T extends IBaseModel> = FetchNextParams & ({
+  status: 'fetching' | 'done'
+} | {
+  status: 'error'
+  error?: ErrorCatched
+} | {
+  status: 'merging'
+  data: T[] | null
+}) & {
+  id: number
+}
+
+type ReturnFFNStatus<T extends 'index' | 'value', M extends IBaseModel> = T extends 'index' ? number : FetchNextStatus<M>
+
+const findFNStatus = <
+  M extends IBaseModel,
+  T extends 'index' | 'value' = 'value'
+>(
+    p: Partial<FetchNextStatus<M>>,
+    l: Array<FetchNextStatus<M>>,
+    t: T, by: 'props' | 'id' = 'props'
+  ): ReturnFFNStatus<T, M> => {
+  const conditionByProps = (i: FetchNextStatus<M>) => i.offset === p.offset && i.limit === p.limit
+  const conditionById = (i: FetchNextStatus<M>) => i.id === p.id
+  const condition = by === 'props' ? conditionByProps : conditionById
+  if (t === 'index') return l.findIndex(condition) as ReturnFFNStatus<T, M>
+  return l.find(condition) as ReturnFFNStatus<T, M>
 }
 
 export const useBase = <Model extends IBaseModel>(params: BaseParams<Model>) => {
@@ -41,11 +80,8 @@ export const useBase = <Model extends IBaseModel>(params: BaseParams<Model>) => 
   // ========== HELPER HOOKS ==========
   const { setAppNetStatus, isAppOffline, isAppNetStatus } = useNetStatus()
   const { debounce } = useDebounce()
-  const [needFetchNext, setNeedFetchNext] = useState(false)
-  const [fetchNextParams, setFetchNextParams] = useState<FetchNextParams>({
-    offset: 0,
-    limit: 10
-  })
+  const [isFetching, setIsFetching] = useState(false)
+  const [fetchNextStatus, setFetchNextStatus] = useState<Array<FetchNextStatus<Model>>>([])
 
   // ========== CONFIG ==========
   const { path, initFetch = true, model } = params
@@ -106,52 +142,117 @@ export const useBase = <Model extends IBaseModel>(params: BaseParams<Model>) => 
 
   const fetchF = async (params: FetchParams<Model>) => {
     const { query, searchBy, mode = 'clean' } = params
-    const res = await (storage.read({
-      query,
-      searchBy
-    }))
-    if (mode === 'merge') {
-      const merged = secureMerge(data, res.data ?? [])
-      setData(merged)
+    setIsFetching(true)
+    try {
+      const res = await (storage.read({
+        query,
+        searchBy
+      }))
+      if (mode === 'merge') {
+        const merged = secureMerge(data, res.data ?? [])
+        setData(merged)
+      }
+      if (mode === 'clean') {
+        setData(res?.data ?? [])
+        setFetchNextStatus([{
+          limit: parseInt(res.queryUsed?.limit ?? '0'),
+          offset: parseInt(res.queryUsed?.offset ?? '0'),
+          status: 'done',
+          id: 0
+        }])
+      }
+      if (mode === 'info') {
+        setIsFetching(false)
+        return res
+      }
+      setMetadata({
+        ...res?.queryUsed,
+        searchByUsed: searchBy
+      })
+      setIsFetching(false)
+      return res
+    } catch (err) {
+      setIsFetching(false)
+      throw err
     }
-    if (mode === 'clean') {
-      setData(res?.data ?? [])
-    }
-    setMetadata(res?.queryUsed)
-    setMetadata({
-      ...res?.queryUsed,
-      searchByUsed: searchBy
-    })
-    return res
   }
 
   const fetch = useCallback(fetchF, [storage, data])
 
-  const fetchNext = useCallback(async () => {
+  const fetchNext = useCallback(async (params: FetchNextParams) => {
     if (metadata == null) return
-    const { limit, offset } = fetchNextParams
+    const { limit, offset } = params
     if (offset > metadata.count) return
     const { searchByUsed, ...rest } = metadata
-    return await fetch({
-      mode: 'merge',
-      query: {
-        ...rest,
-        offset: String(offset),
-        limit: String(limit)
-      },
-      searchBy: searchByUsed
-    })
-  }, [fetch, metadata, fetchNextParams])
 
-  const launchNextFetch = (params?: Partial<{
+    const fnsIndex = findFNStatus(params, fetchNextStatus, 'index')
+    if (fnsIndex > 0) return fetchNextStatus[fnsIndex]
+
+    const length = fetchNextStatus.length
+    const id = length > 0 ? fetchNextStatus[length - 1].id : 0
+    const fns: FetchNextStatus<Model> = {
+      ...params,
+      id,
+      status: 'fetching'
+    }
+    setFetchNextStatus([...fetchNextStatus, fns])
+
+    try {
+      const res = await fetch({
+        mode: 'info',
+        query: {
+          ...rest,
+          offset: String(offset),
+          limit: String(limit)
+        },
+        searchBy: searchByUsed
+      })
+
+      const fnsIndex = findFNStatus(params, fetchNextStatus, 'index')
+      const old = fnsIndex > 0 ? fetchNextStatus[fnsIndex] : params
+      const length = fetchNextStatus.length
+      const id = length > 0 ? fetchNextStatus[length - 1].id : 0
+      const fns: FetchNextStatus<Model> = {
+        ...old,
+        id,
+        status: 'merging',
+        data: res.data
+      }
+      setFetchNextStatus([...fetchNextStatus, fns])
+      setMetadata({
+        ...res.queryUsed,
+        searchByUsed
+      })
+      return fns
+    } catch (err) {
+      const fnsIndex = findFNStatus(params, fetchNextStatus, 'index')
+      const old = fnsIndex > 0 ? fetchNextStatus[fnsIndex] : params
+      const length = fetchNextStatus.length
+      const id = length > 0 ? fetchNextStatus[length - 1].id : 0
+      const fns: FetchNextStatus<Model> = {
+        ...old,
+        id,
+        status: 'error',
+        error: err as any
+      }
+      setFetchNextStatus([...fetchNextStatus, fns])
+      return fns
+    }
+  }, [fetch, metadata, fetchNextStatus])
+
+  const launchNextFetch = useCallback((params?: Partial<{
     limit?: FetchNextParams['limit']
     offset?: FetchNextParams['offset'] | 'auto' | 'previous'
   }>) => {
-    const { offset: previousOffset, limit: previousLimit } = fetchNextParams
+    const fnsPreIndex = fetchNextStatus.length - 1
+    const defaultFNS = { limit: 10, offset: -10 }
+    const previous = fnsPreIndex < 0 ? defaultFNS : fetchNextStatus[fnsPreIndex]
+    const { offset: previousOffset, limit: previousLimit } = previous
     const { offset: newOffset, limit: newLimit } = params ?? {
       limit: undefined,
       offset: undefined
     }
+
     const makeParams = () => {
       const limit = newLimit ?? previousLimit
       if (typeof newOffset === 'undefined' || newOffset === 'auto') {
@@ -168,9 +269,9 @@ export const useBase = <Model extends IBaseModel>(params: BaseParams<Model>) => 
       }
       return { limit, offset: newOffset }
     }
-    setFetchNextParams(makeParams())
-    setNeedFetchNext(true)
-  }
+
+    void fetchNext(makeParams())
+  }, [fetchNext, fetchNextStatus])
 
   // ========================================
   // ============= UPDATE HOOKS =============
@@ -195,16 +296,32 @@ export const useBase = <Model extends IBaseModel>(params: BaseParams<Model>) => 
     }
   }
 
-  const configFetchNext = () => {
-    if (!needFetchNext) return
-    void fetchNext().then(() => {
-      setNeedFetchNext(false)
+  const configFNSMerger = () => {
+    const toMerge = fetchNextStatus.filter((s) => s.status === 'merging')
+    if (toMerge.length < 1) return
+
+    const finalData: Model[] = []
+    const toUpdate: Array<FetchNextStatus<Model>> = toMerge.map((item) => {
+      if (item.status !== 'merging') return item
+      const { data, ...rest } = item
+      finalData.push(...data ?? [])
+      return {
+        ...rest,
+        status: 'done'
+      }
     })
+
+    const finalStatus = fetchNextStatus.map(({ id }) => {
+      return findFNStatus({ id }, toUpdate, 'value', 'id')
+    })
+    const merge = secureMerge(data, finalData)
+    setFetchNextStatus(finalStatus)
+    setData(merge)
   }
 
   useEffect(configInitFetching, [initFetch, offline, fetch, isInit])
   useEffect(configAppMode, [offline, storage, isAppNetStatus, setAppNetStatus, debounce])
-  useEffect(configFetchNext, [needFetchNext, fetchNext, fetchNextParams])
+  useEffect(configFNSMerger, [fetchNextStatus, data])
 
   // ============================================
   // ============= CREATE FUNCTIONS =============
@@ -264,8 +381,8 @@ export const useBase = <Model extends IBaseModel>(params: BaseParams<Model>) => 
     findBy,
     fetch,
     launchNextFetch,
-    fetchNextParams,
-    needFetchNext,
+    needFetchNext: isFetching,
+    isFetching,
     create,
     update,
     delete: remove
